b45e325335185db660f8a7e85380f82035bdeacd
Nicholas Philo <67591185+nicholasphilo@users.noreply.github.com>
2020-07-22 14:42:23 +0100

    Tidied the data

--- run_analysis.R
+++ run_analysis.R
@@ -48,15 +48,51 @@ sj_data <- rbind(sj_train, sj_test)
 
 rm("x_train","y_train","sj_train","x_test","y_test","sj_test")
 
-### Reading in the feature data and activity label data:
+### Reading in the feature information and activity label data:
 
 feature <- read.table("./UCI HAR Dataset/features.txt")
 actv_label <- read.table( "./UCI HAR Dataset/activity_labels.txt")
-actv_label[,2] <- as.character(a_label[,2])
+actv_label[,2] <- as.character(actv_label[,2])
 
 ### Identifying column names with mean or std in them 
 
 mean_std_condition <- grep("-(mean|std).*"  ,  as.character(feature[,2]))
+dta_names <- as.character(  feature[  mean_std_condition  ,2]  )
+
+
+
+### selecting mean and std columns of x_data using the mean_std_condition
+
+x_data <- x_data[,mean_std_condition]
+
+### Combining the data sets
+
+data <- cbind.data.frame(  sj_data  ,  y_data  ,  x_data  )
+colnames(data) <- c("subject", "activity", dta_names)
+
+### Creating factor variables for the subject and activity variables
+
+data$activity <- factor(data$activity, levels = actv_label[,1], labels = actv_label[,2])
+data$subject <- as.factor(data$subject)
+
+
+### Tidying the dta_names then renaming columns of data
+
+dta_names <- gsub("^t", "Time", dta_names)
+dta_names <- gsub("^f", "Frequency", dta_names)
+dta_names <- gsub("-", " ", dta_names)
+dta_names <- gsub("mean\\(\\)", "Mean", dta_names)
+dta_names <- gsub("std\\(\\)", "Standard Deviation", dta_names)
+dta_names <- gsub("Body", " Body", dta_names)
+dta_names <- gsub("Gyro", " Gyroscope", dta_names)
+dta_names <- gsub("Acc", " Accelerometer", dta_names)
+dta_names <- gsub("Mag", " Magnitude", dta_names)
+dta_names <- gsub("Jerk", " Jerk", dta_names)
+dta_names <- gsub("Gravity", " Gravity", dta_names)
+dta_names <- gsub("Body Body", "Body", dta_names)
+dta_names <- tolower(dta_names)
+
+colnames(data) <- c("subject", "activity", dta_names)
 
 
 
